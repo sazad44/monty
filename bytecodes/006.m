fd1b8a57eca066018823e06b4a659e23b52a659e
sazad44 <530@holbertonschool.com>
2018-12-20 17:04:32 +0000

    Updating functions to skip lines with spaces

---
+++ bytecodes/006.m
@@ -0,0 +1,35 @@
+nop
+pop
+push 2323234  push 23
+   
+   
+   
+push 1
+    
+push 2
+
+pint
+
+
+
+pall
+
+
+              hi        
+
+
+pop
+
+
+
+pint
+
+
+
+pall
+
+
+
+
+
+
--- op_functions.c
+++ op_functions.c
@@ -72,3 +72,10 @@ void pop(stack_t **stack, unsigned int line_number)
 		*stack = NULL;
 	}
 }
+
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	return;
+}
