55a2b06a2f254442ac52add2b77f181197962aa3
Rashaad Colbert <colbertrashaad@gmail.com>
2018-12-21 05:43:21 +0000

    Adding mul and div functionality to monty

--- op_functions2.c
+++ op_functions2.c
@@ -62,3 +62,55 @@ void sub(stack_t **stack, unsigned int line_number)
 	free((*stack)->prev);
 	(*stack)->prev = NULL;
 }
+
+/**
+ * div - divides top two elements in the stack
+ * @stack: pointer to a pointer to the top of the stack
+ * @line_number: line number
+ * Return: No Value
+*/
+void div(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		free(glo->ipt), free(glo->tokop), free(glo);
+		if (*stack)
+			free_stack(*stack);
+		fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	else if ((*stack)->n == 0)
+	{
+		free(glo->ipt), free(glo->tokop), free(glo);
+		if (*stack)
+			free_stack(*stack);
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}		
+	*stack = (*stack)->next;
+	(*stack)->n = (*stack)->n / (*stack)->prev->n;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
+
+/**
+ * mul - multiplies top two elements in the stack
+ * @stack: pointer to a pointer to the top of the stack
+ * @line_number: line number
+ * Return: No Value
+*/
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		free(glo->ipt), free(glo->tokop), free(glo);
+		if (*stack)
+			free_stack(*stack);
+		fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	*stack = (*stack)->next;
+	(*stack)->n = (*stack)->n * (*stack)->prev->n;
+	free((*stack)->prev);
+	(*stack)->prev = NULL;
+}
