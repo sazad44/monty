9fad3c932f321948152f41ca0faae3cbff655440
sazad44 <530@holbertonschool.com>
2019-02-08 06:25:52 +0000

    Updating exe helpers

--- exe_helpers.c
+++ exe_helpers.c
@@ -12,7 +12,7 @@ int bc_exe(char *ipt, stack_t **stack)
 	instruction_t instarr[] = {
 		{"push", push}, {"pall", pall},	{"pint", pint}, {"pop", pop},
 		{"nop", nop}, {"swap", swap}, {"add", add}, {"sub", sub}, {"div", _div},
-		{"mul", mul}, {"mod", mod}, {"pchar", pchar}, {"pstr", pstr}, {NULL, NULL}
+		{"mul", mul}, {"mod", mod}, {"pchar", pchar}, {"pstr", pstr},e {NULL, NULL}
 	};
 	char *tok;
 
